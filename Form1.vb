f4f1ac9757934252f6f9c1a4e45973e5344687d3
astouffer23 <astouffer@gmail.com>
2022-05-12 08:15:23 -0500

    1
     1

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports GroupDocs.Merger
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim licensePath2 As String = "C:\Temp\GroupDocs.Merger.NET.lic"
+        Dim lic2 As GroupDocs.Merger.License = New GroupDocs.Merger.License()
+        Using merger = New Merger("c:\temp\1.pdf")
+            merger.Save("C:\temp\testMerge.pdf")
+        End Using
+    End Sub
+End Class
